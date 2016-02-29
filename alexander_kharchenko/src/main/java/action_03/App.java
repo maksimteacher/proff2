70cdfacff1d413659ccf046372873cdb0b6173e2
Alexandr <Alexandr@Alexandr-PC>
2016-02-29 21:20:57 +0200

    1111

--- alexander_kharchenko/src/main/java/action_03/App.java
+++ alexander_kharchenko/src/main/java/action_03/App.java
@@ -6,6 +6,6 @@ package action_03;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!  ererer ");
+		System.out.println("Hello World!  privet ");
 	}
 }
