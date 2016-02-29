1884c13f629197b9f80e5dd67805d17a2cb4f2e6
Name1 <email>
2016-03-01 01:24:24 +0200

    normal App.java

--- proff2_example/src/main/java/action03/App.java
+++ proff2_example/src/main/java/action03/App.java
@@ -6,6 +6,6 @@ package action03;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello People and Animals!");
+		System.out.println("Hello People!");
 	}
 }
