a43a27865bf493afed04495f0d9c32bddf7d4db0
natasur <natasur-93@mail.ru>
2016-03-10 21:40:44 +0200

    lesson

---
+++ nata_sur/src/main/java/action05/Task1.java
@@ -0,0 +1,25 @@
+package action05;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.mysql.fabric.xmlrpc.base.Array;
+
+//Рефлексия. Задача1: Вывести все методы класса String
+
+public class Task1 {
+	public static void main(String[] args) {
+		String obj = "fghjkl";
+		Class cl = obj.getClass();
+		Method[] m = cl.getMethods();
+		//System.out.println(Arrays.toString(m));
+		for (Method method : m) {
+			//System.out.println(method);
+		//System.out.println(method.getName());
+			System.out.println(method.getName() + " " + method.getReturnType().getName());
+		}
+
+	}
+
+}
