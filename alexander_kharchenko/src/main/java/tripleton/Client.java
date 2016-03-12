df2dd98f05ec9d89573462c58002f100d1cad46d
Alexandr <Alexandr@Alexandr-PC>
2016-03-12 20:29:05 +0200

    tripleton update

--- alexander_kharchenko/src/main/java/tripleton/Client.java
+++ alexander_kharchenko/src/main/java/tripleton/Client.java
@@ -2,23 +2,34 @@ package tripleton;
 
 public class Client {
 	private static int counter = 3;
-	private static Client lastClient;
+	private static Client[] clients;
 	private int id;
 	
-	private Client() {
-		id = counter--;
+	static {
+		clients = new Client[counter];
+		for (int i = 0 ; i < counter ; i++) {
+			clients[i] = new Client(i);
+		}
 	}
 	
+	private Client(int id) {
+		this.id = id;
+	}
+	
+	private Client(){}
+	
 	public static Client getInstance() {
-		if (counter > 0) {
-			lastClient = new Client();
-		}
-		return lastClient;
+		int n = (int) (Math.random() * counter);
+		return clients[n];
 	}
 
 	@Override
 	public String toString() {
 		return "Client [id=" + id + "]";
+	}
+
+	public int getId() {
+		return id;
 	}
 	
 	
--- alexander_kharchenko/src/main/java/tripleton/Runner.java
+++ alexander_kharchenko/src/main/java/tripleton/Runner.java
@@ -7,11 +7,16 @@ public class Runner {
 
 	public static void main(String[] args) {
 		List<Client> list = new ArrayList<>();
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 150; i++) {
 			list.add(Client.getInstance());
 		}
+		int [] frequency = new int[3];
 		for (Client client : list) {
 			System.out.println(client);
+			frequency[client.getId()]++;
+		}
+		for (int i : frequency) {
+			System.out.print(i + " ");
 		}
 		
 	}
