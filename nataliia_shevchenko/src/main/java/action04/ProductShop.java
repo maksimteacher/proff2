497a205b21551bda49816de796bf46aad4e7b304
Nataly <Nataly@Nataly>
2016-03-14 20:02:01 +0200

    new updates(it includes homeworks and work at the lessons)

---
+++ nataliia_shevchenko/src/main/java/action04/ProductShop.java
@@ -0,0 +1,13 @@
+package action04;
+
+import java.util.Map;
+
+
+public class ProductShop {
+	//Map<Product, String> shop(Product, NameOfConstructor)
+	public static void main(String[] args) {
+		Map<Product, String> contructors;
+
+	}
+
+}
