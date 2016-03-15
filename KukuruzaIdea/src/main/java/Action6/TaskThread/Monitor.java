d0ad9e42863b8ede265cb9807dc9693b66f4ae13
KukuruzaAndrey <andreyku6@gmail.com>
2016-03-15 11:08:01 +0200

    test git

---
+++ KukuruzaIdea/src/main/java/Action6/TaskThread/Monitor.java
@@ -0,0 +1,15 @@
+package Action6.TaskThread;
+
+public class Monitor {
+    private String nameFile1, nameFile2;
+    private ThreadWriter threadWriter;
+    private ThreadCreator threadCreator;
+    private Object sync = new Object();
+
+    public Monitor(String nameFile1, String nameFile2) {
+        this.nameFile1 = nameFile1;
+        this.nameFile2 = nameFile2;
+        threadCreator =
+    }
+
+}
