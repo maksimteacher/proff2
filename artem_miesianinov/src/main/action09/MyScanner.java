eb09721ccf5481939e41a3ef8d70a2f0a5502408
Miesianinov <mesianynov@gmail.com>
2016-03-29 14:06:30 +0300

    action09 commit

--- artem_miesianinov/src/main/action09/MyScanner.java
+++ artem_miesianinov/src/main/action09/MyScanner.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-public class MyScaner {
+public class MyScanner {
 
 	public static void main(String[] args) throws IOException{
 		Reader sysReader = new InputStreamReader (System.in);
