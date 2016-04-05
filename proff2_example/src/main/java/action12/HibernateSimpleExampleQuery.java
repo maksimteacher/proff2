5de6593d37e880fcf0ceb10caad028f5a716a13c
Maks Coach <maks_coach@ukr.net>
2016-04-05 21:51:28 +0300

    SQL and HQL

--- proff2_example/src/main/java/action12/HibernateSimpleExampleQuery.java
+++ proff2_example/src/main/java/action12/HibernateSimpleExampleQuery.java
@@ -24,24 +24,28 @@ public class HibernateSimpleExampleQuery {
 		Query query = null;
 		List<Product> list = null;
 		try {
+			session = factory.openSession();
 			//
-			query = session.createSQLQuery("select * from Product where code between :a and :b")
-				.addEntity(Product.class);
-				query.setInteger("a", 16);
-				query.setInteger("b", 17);
-				list = query.list();
-			//
-			query = session.createSQLQuery("select name from Product where id > ?")
-			.addEntity(Product.class);
-			query = query.setLong(0, 2);
+			query = session.createSQLQuery("select * from product where code between :a and :b")
+					.addEntity(Product.class);
+			query.setInteger("a", 2);
+			query.setInteger("b", 7);
 			list = query.list();
+			System.out.println("1="+list);
 			//
+			query = session.createSQLQuery("select name from Product where ? < id or id < ?");
+			query = query.setLong(0, 2);
+			query = query.setLong(1, 4);
+			List<String> str = query.list();
+			System.out.println("2="+str);
+			// HQL
 			query = session.createQuery("from Product");
 			list = query.list();
+			System.out.println("3="+list);
 			//
 			query = session.createQuery("select barcode from Product");
-			List<String>  listBarcode = query.list();
-			
+			List<String> listBarcode = query.list();
+			System.out.println("4="+listBarcode);
 		} catch (HibernateException e) {
 			log.error("Open session failed", e);
 		} finally {
