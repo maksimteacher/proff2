c077f20ecb4ff0164dc037b42dbc31d4a37f5015
Maks Coach <maks_coach@ukr.net>
2016-05-24 20:11:28 +0300

    update

--- proff2_example_game1/src/org/start/CanvasActivity_SurfaceView.java
+++ proff2_example_game1/src/org/start/CanvasActivity_SurfaceView.java
@@ -4,60 +4,85 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 
 public class CanvasActivity_SurfaceView extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(new DrawView(this));
-	}
 
-	class DrawView extends View {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(new DrawView(this));
+  }
 
-		public DrawView(Context context) {
-			super(context);
-		}
+  class DrawView extends SurfaceView implements SurfaceHolder.Callback {
 
-		@Override
-		protected void onDraw(Canvas canvas) {
-			canvas.drawColor(Color.GREEN);
-			new DrawThread(canvas).start();
-		}
+    private DrawThread drawThread;
 
-		class DrawThread extends Thread {
-			Canvas canvas;
-			private int xPos = 1;
-			private int xDelta = 1;
-			private int yPos = 1;
-			private Paint pointPaint = new Paint(Color.GREEN);
-			private Paint emptyPaint = new Paint(Color.WHITE);
-			
-			public DrawThread(Canvas canvas) {
-				this.canvas = canvas;
-				pointPaint.setStrokeWidth(10);
-				emptyPaint.setStrokeWidth(10);
-			}
+    public DrawView(Context context) {
+      super(context);
+      getHolder().addCallback(this);
+    }
 
-			public void run() {
-				while (true) {
-					canvas.drawPoint(xPos, yPos, emptyPaint);
-					xPos += xDelta;
-					canvas.drawPoint(xPos, yPos, pointPaint);
-					try {
-						sleep(100);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-					invalidate();
-				}
-			}
-		}
-	}
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+        int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+      drawThread = new DrawThread(getHolder());
+      drawThread.setRunning(true);
+      drawThread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+      boolean retry = true;
+      drawThread.setRunning(false);
+      while (retry) {
+        try {
+          drawThread.join();
+          retry = false;
+        } catch (InterruptedException e) {
+        }
+      }
+    }
+
+    class DrawThread extends Thread {
+
+      private boolean running = false;
+      private SurfaceHolder surfaceHolder;
+
+      public DrawThread(SurfaceHolder surfaceHolder) {
+        this.surfaceHolder = surfaceHolder;
+      }
+
+      public void setRunning(boolean running) {
+        this.running = running;
+      }
+
+      @Override
+      public void run() {
+        Canvas canvas;
+        while (running) {
+          canvas = null;
+          try {
+            canvas = surfaceHolder.lockCanvas(null);
+            if (canvas == null)
+              continue;
+            canvas.drawColor(Color.GREEN);
+          } finally {
+            if (canvas != null) {
+              surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+          }
+        }
+      }
+    }
+
+  }
+
 }
